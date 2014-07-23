853f013d3fc0060c513c0280a59c1a8ed67e549e
Gautham Rayavarapu <cod492@gmail.com>
2014-07-23 02:27:50 -0400

    Database changes Huge
    include add, update, delete, getAll functionality

--- src/com/fydp/cpsapp/CPSDatabaseHelper.java
+++ src/com/fydp/cpsapp/CPSDatabaseHelper.java
@@ -2,8 +2,15 @@
  * 
  */
 package com.fydp.cpsapp;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.*;
+import android.util.Log;
 
 /**
  * @author Gautham
@@ -18,6 +25,22 @@ public class CPSDatabaseHelper extends SQLiteOpenHelper{
 
 	//Parking System Data table name
 	private static final String TABLE_PSD = "PSD";
+	
+	//Parking System Data columns
+	private static final String KEY_ID = "id";
+	private static final String KEY_USER = "cps_user_id";
+	private static final String KEY_DEVICE = "device_id";
+	private static final String KEY_STATE = "tap_type";
+	private static final String KEY_LAT = "latitude";
+	private static final String KEY_LONG = "longitude";
+	private static final String KEY_LOC = "location";
+	private static final String KEY_REG = "zonal_reg";
+	private static final String KEY_COST = "cost";
+	private static final String KEY_TIME = "timeStamp";
+	
+	//columns
+	private static final String[] COLUMNS_PSD = {KEY_ID,KEY_USER,KEY_DEVICE,KEY_STATE,KEY_LAT,
+												KEY_LONG,KEY_LOC,KEY_REG,KEY_COST,KEY_TIME};
 	
 	public CPSDatabaseHelper(Context context){
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -49,5 +72,154 @@ public class CPSDatabaseHelper extends SQLiteOpenHelper{
 			System.out.println(e.getMessage());
 		}
 	}
+	
+	public void addCPSData(CPSData data){
+		//logging
+		Log.d("addData", data.toString());
+		
+		//1. get reference to writable db
+		SQLiteDatabase db = this.getWritableDatabase();
+		
+		//2. create ContentValues to add key "column"/value
+		ContentValues values = new ContentValues();
+		values.put(KEY_USER, data.getUserID());
+		values.put(KEY_DEVICE, data.getDeviceID());
+		values.put(KEY_STATE, data.getTapType());
+		values.put(KEY_LAT, data.getLatitude());
+		values.put(KEY_LONG, data.getLongitude());
+		values.put(KEY_LOC, data.getLocation());
+		values.put(KEY_REG, data.getZonalReg());
+		values.put(KEY_COST, data.getCost());
+		values.put(KEY_TIME, data.getTimeStamp());
+		
+		//3. insert the data into the db
+		try{
+			db.insert(TABLE_PSD, null, values);
+		}catch(SQLiteException e) {
+			System.out.println(e.getMessage());
+		}
+		db.close();
+	}
 
+	public CPSData getCPSData(int id){
+		//1. get reference to readable DB
+		SQLiteDatabase db  = this.getReadableDatabase();
+		
+		//2. build query 
+		Cursor cursor = 
+	            db.query(TABLE_PSD, // a. table
+	            COLUMNS_PSD, // b. column names
+	            " id = ?", // c. selections 
+	            new String[] { String.valueOf(id) }, // d. selections args
+	            null, // e. group by
+	            null, // f. having
+	            null, // g. order by
+	            null); // h. limit
+	 
+	    // 3. if we got results get the first one
+	    if (cursor != null)
+	        cursor.moveToFirst();
+	 
+	    // 4. build CPSData object
+	    CPSData data = new CPSData();
+	    data.setId(Integer.parseInt(cursor.getString(0)));
+	    data.setUserID(cursor.getString(1));
+	    data.setDeviceID(cursor.getString(2));
+	    data.setTapType(cursor.getString(3));
+	    data.setLatitude(cursor.getString(4));
+	    data.setLongitude(cursor.getString(5));
+	    data.setLocation(cursor.getString(5));
+	    data.setZonalReg(cursor.getString(6));
+	    data.setCost(cursor.getString(7));
+	    data.setTimeStamp(cursor.getString(8));
+	   
+	    //log 
+	    Log.d("getBook("+id+")", data.toString());
+	 
+	    // 5. return book
+	    return data;
+	}
+	
+	public List<CPSData> getAllCPSData() {
+	       List<CPSData> allData = new LinkedList<CPSData>();
+	 
+	       // 1. build the query
+	       String selectQuery = "SELECT  * FROM " + TABLE_PSD;
+	 
+	       // 2. get reference to writable DB
+	       SQLiteDatabase db = this.getWritableDatabase();
+	       Cursor cursor = db.rawQuery(selectQuery, null);
+	 
+	       // 3. go over each row, build book and add it to list
+	       CPSData dataEntry = null;
+	       if (cursor.moveToFirst()) {
+	           do {
+	               dataEntry = new CPSData();
+	               dataEntry.setId(Integer.parseInt(cursor.getString(0)));
+		       	   dataEntry.setUserID(cursor.getString(1));
+		       	   dataEntry.setDeviceID(cursor.getString(2));
+		       	   dataEntry.setTapType(cursor.getString(3));
+		       	   dataEntry.setLatitude(cursor.getString(4));
+		       	   dataEntry.setLongitude(cursor.getString(5));
+		       	   dataEntry.setLocation(cursor.getString(5));
+		       	   dataEntry.setZonalReg(cursor.getString(6));
+		       	   dataEntry.setCost(cursor.getString(7));
+		       	   dataEntry.setTimeStamp(cursor.getString(8));
+	 
+	               // Add book to books
+	               allData.add(dataEntry);
+	           } while (cursor.moveToNext());
+	       }
+	 
+	       Log.d("getAllBooks()", allData.toString());
+	 
+	       // return books
+	       return allData;
+	   }
+	
+	public int updateCPSData(CPSData data){
+	    // 1. get reference to writable DB
+	    SQLiteDatabase db = this.getWritableDatabase();
+	 
+	    // 2. create ContentValues to add key "column"/value
+	    ContentValues values = new ContentValues();
+	    values.put(KEY_USER, data.getUserID());
+		values.put(KEY_DEVICE, data.getDeviceID());
+		values.put(KEY_STATE, data.getTapType());
+		values.put(KEY_LAT, data.getLatitude());
+		values.put(KEY_LONG, data.getLongitude());
+		values.put(KEY_LOC, data.getLocation());
+		values.put(KEY_REG, data.getZonalReg());
+		values.put(KEY_COST, data.getCost());
+		values.put(KEY_TIME, data.getTimeStamp());
+		
+	 
+	    // 3. updating row
+	    int i = db.update(TABLE_PSD, //table
+	            values, // column/value
+	            KEY_ID+" = ?", // selections
+	            new String[] { String.valueOf(data.getId()) }); //selection args
+	 
+	    // 4. close
+	    db.close();
+	 
+	    return i;
+	}
+	
+	public void deleteCPSData(CPSData data) {
+		 
+        // 1. get reference to writable DB
+        SQLiteDatabase db = this.getWritableDatabase();
+ 
+        // 2. delete
+        db.delete(TABLE_PSD, //table name
+                KEY_ID+" = ?",  // selections
+                new String[] { String.valueOf(data.getId()) }); //selections args
+ 
+        // 3. close
+        db.close();
+ 
+        //log
+        Log.d("deleteBook", data.toString());
+    }
 }
